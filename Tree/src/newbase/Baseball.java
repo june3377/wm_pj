328aa2137279a7a26c54752d89fcb558616de006
Park <june3377@nate.com>
2017-01-17 16:31:55 +0900

    재업데이트 모듈화 및 기능 추가

---
+++ Tree/src/newbase/Baseball.java
@@ -0,0 +1,100 @@
+package newbase;
+
+import java.util.HashMap;
+
+public class Baseball {
+
+	private int try_cnt;
+	private int strike_cnt;
+	private int ball_cnt;
+	private int[] rd_num;
+	private HashMap<Integer, String> hm = new HashMap();
+	private int rank;
+
+	public Baseball() {
+		this.try_cnt = 0;
+		this.strike_cnt = 0;
+		this.ball_cnt = 0;
+		this.rd_num = new int[3];
+	}
+
+	public void ball(int realnum[]) {
+		int ball = 0;
+		for (int i = 0; i < rd_num.length; i++) {
+			for (int j = 0; j < rd_num.length; j++) {
+				if (i == j)
+					continue;
+				else {
+					if (rd_num[i] == realnum[j]) {
+						ball++;
+					}
+				}
+			}
+		}
+		ball_cnt = ball;
+	}
+
+	public void strike(int realnum[]) {
+		int strike = 0;
+		for (int i = 0; i < rd_num.length; i++) {
+			if (rd_num[i] == realnum[i]) {
+				strike++;
+			}
+		}
+		strike_cnt = strike;
+	}
+
+	public void random() {
+		while (true) {
+			int num1 = (int) (Math.random() * 10);
+			int num2 = (int) (Math.random() * 10);
+			int num3 = (int) (Math.random() * 10);
+			if (num1 != num2 && num3 != num1 && num3 != num2) { // 중복안되게
+				rd_num[0] = num1;
+				rd_num[1] = num2;
+				rd_num[2] = num3;
+				break;
+			}
+		}
+	}
+
+	public void b_try() {
+		try_cnt++;
+	}
+
+	public int getTry_cnt() {
+		return try_cnt;
+	}
+
+	public int getStrike_cnt() {
+		return strike_cnt;
+	}
+
+	public int getBall_cnt() {
+		return ball_cnt;
+	}
+
+	public void getRd_num() {
+		System.out.println("답은" + rd_num[0] + "" + rd_num[1] + "" + rd_num[2]);
+	}
+
+	public String getHm() {
+		if(hm.get(1) == null) return hm.get(0);
+		if(hm.get(2) == null) return hm.get(0) + hm.get(1);
+		return hm.get(0)+","+ hm.get(1)+","+ hm.get(2);
+	}
+
+	public void setHm(String name) {
+		this.hm.put(rank, name);
+	}
+	
+	public void rank_up(){
+		if(rank==3){
+			rank=0;
+		}
+		rank++;
+	}
+	public void newgame(){
+		this.try_cnt=0;
+	}
+}
---
+++ Tree/src/newbase/NewBaseball2.java
@@ -0,0 +1,68 @@
+package newbase;
+import java.util.Scanner;
+
+public class NewBaseball2 {
+	static int[] realnum = new int[3];
+	static Baseball tt = new Baseball();
+	
+	public static void main(String[] args) {
+		tt.random();
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("########################################################");
+		System.out.println("#                   play ball                          #");
+		System.out.println("########################################################");
+		System.out.println();
+
+		while (true) {
+			System.out.print("숫자 3개를 입력해주세요 ex(1 2 3)");
+			String line = scanner.nextLine();
+			String[] num = line.split(" ");
+
+			for (int i = 0; i < realnum.length; i++) {
+				realnum[i] = Integer.parseInt(num[i]);
+			} // for문 끝
+
+			if ( realnum[0] == realnum[1] && realnum[2] == realnum[0] && realnum[2] == realnum[1]) {
+				System.out.println("중복된 숫자를 입력하셨습니다.");
+				continue;
+			}
+					
+			/*tt.getRd_num();*/
+			tt.ball(realnum);
+			tt.strike(realnum);
+			
+			System.out.println(" 스트라이크 " + tt.getStrike_cnt() + " 볼 " + tt.getBall_cnt());
+			System.out.println();
+			
+			tt.b_try();
+
+			if (tt.getStrike_cnt() == 3) {
+				System.out.println("당신은 " + tt.getTry_cnt() + "번만에 맞추셨습니다.");
+				tt.random();
+				System.out.println("축하합니다 ^^");
+				System.out.println("계속하시겠습니까? y/n 정답보면서하기 cheat");
+				
+				String input = scanner.nextLine();
+				
+				if (input.equals("n")){
+					System.out.println("당신의 이름은?");
+					tt.setHm(scanner.nextLine());
+					System.out.println("플레이어 명단");
+					System.out.println(tt.getHm());
+					break;
+				}else if(input.equals("y")){
+					System.out.println("당신의 이름은?");
+					tt.setHm(scanner.nextLine());
+					tt.rank_up();
+					tt.newgame();
+					System.out.println("등록 되었습니다 ^^");
+				}else if(input.equals("cheat")){
+					tt.getRd_num();
+					tt.newgame();
+					
+				}
+			}
+		}
+	}
+}
